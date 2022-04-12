f187ea22d4b8a933aebeaf5d2f0f267f6a6a74d7
axidex <Stepworm@yandex.ru>
2022-04-12 14:12:17 +0300

    builder

--- builder.sh
+++ builder.sh
@@ -1,10 +1,19 @@
-mkdir build
+BASE_DIR=$(dirname "$0")
+BASE_DIR="$PWD/$BASE_DIR"
+BUILD_DIR="build"
+rm -rf $BUILD_DIR
+
+if [ ! -d $BUILD_DIR ]; then
+        mkdir $BUILD_DIR;
+fi
 
 vars_link=$1
 
+cd $BASE_DIR/$BUILD_DIR
+
 if [ "$vars_link" = "Lec23" ]; then
-    {
-        cd build
-        CMake ../Lec23
-    }
+    cmake $BASE_DIR/Lec23
+
+elif [ "$vars_link" = "Lec4" ]; then
+    cmake $BASE_DIR/Lec4
 fi
